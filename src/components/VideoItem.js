04ff7a339046d3d61cc08b941c04d6a426f69a06
MyGitHub-11 <74892310+MyGitHub-11@users.noreply.github.com>
2021-10-31 16:56:32 +0530

    first commit

---
+++ src/components/VideoItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Grid, Paper, Typography } from "@material-ui/core";
+
+const VideoItem = ({ video, onVideoSelect }) => {
+  return (
+    <Grid item xs={18}>
+      <Paper 
+      style={{ display: "flex", alignItems: "center", cursor: "pointer" }} 
+      onClick={() => onVideoSelect(video)} 
+      >
+      <img 
+        style={{ marginRight: "20px" }} 
+        alt="thumbnail" 
+        src={video.snippet.thumbnails.medium.url} 
+      />
+      <Typography variant="subtitle2">
+        <b>{video.snippet.title}</b>
+      </Typography>
+      </Paper>
+    </Grid>
+  );
+}
+
+export default VideoItem;
